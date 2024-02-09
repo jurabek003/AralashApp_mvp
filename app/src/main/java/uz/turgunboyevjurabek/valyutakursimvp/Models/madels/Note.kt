f26974805af9ec0126ed90a009bc431b5845c68d
Asus <turgunboyevjurabek777@gmail.com>
2024-02-09 17:53:26 +0500

    extendFab added and building...

--- app/src/main/java/uz/turgunboyevjurabek/valyutakursimvp/Models/madels/Note.kt
+++ app/src/main/java/uz/turgunboyevjurabek/valyutakursimvp/Models/madels/Note.kt
@@ -3,10 +3,23 @@ package uz.turgunboyevjurabek.valyutakursimvp.Models.madels
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Entity
-data class Note(
-    @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "first_name") val firstName: String?,
-    @ColumnInfo(name = "last_name") val lastName: String?
-)
+ class Note {
+    @PrimaryKey(autoGenerate = true)
+    var uid: Int?=null
+    @ColumnInfo(name = "note_text")
+    var noteText: String?=null
+    @ColumnInfo(name = "time")
+    var time: String? = SimpleDateFormat("HH:mm").format(Date())
+    @ColumnInfo(name = "date")
+    var date: String? = SimpleDateFormat("EEEE", Locale.getDefault()).format(Date())
+
+    constructor(noteText: String?) {
+        this.noteText = noteText
+    }
+
+}
